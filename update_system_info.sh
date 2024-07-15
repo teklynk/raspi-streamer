dda3f5b0424dcd60c271819ca308e7e133fee33c
teklynk <teklynk@protonmail.com>
2024-07-15 17:17:31 -0400

    adjust textarea size. added lsusb to system info

--- update_system_info.sh
+++ update_system_info.sh
@@ -6,6 +6,11 @@ OUTPUT_FILE="system_info.txt"
 # Truncate the output file
 > "$OUTPUT_FILE"
 
+# Run lsub and append the output to the file
+echo "Output of lsusb:" >> "$OUTPUT_FILE"
+lsusb >> "$OUTPUT_FILE"
+echo -e "\n" >> "$OUTPUT_FILE"
+
 # Run arecord -l and append the output to the file
 echo "Output of arecord -l:" >> "$OUTPUT_FILE"
 arecord -l >> "$OUTPUT_FILE"
