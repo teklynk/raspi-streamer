b49bb315e54a29b32dcf7f57cdf695c28dccf4d0
teklynk <teklynk@protonmail.com>
2024-12-26 22:02:38 -0500

    added convert script to convert files to mp4 for file streaming

---
+++ convert.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+convert_file() {
+    local input_file="$1"
+    local filename=$(basename "$input_file")
+    local output_dir="$HOME/raspi-streamer/media"
+    local output_file="$output_dir/${filename%.*}.mp4"
+
+    # Create the output directory if it doesn't exist
+    mkdir -p "$output_dir"
+
+    # Convert video using ffmpeg
+    #ffmpeg -i "$input_file" -c:v libx264 -preset ultrafast -crf 23 -c:a aac -b:a 160k "$output_file"
+    #ffmpeg -i "$input_file" -vf scale=1280:-2 -c:v libx264 -preset slow -crf 23 -c:a aac -b:a 96k "$output_file"
+    ffmpeg -i "$input_file" -vf scale=1280:-2 -c:v libx264 -preset veryfast -crf 23 -c:a aac -b:a 64k "$output_file"
+    echo "Conversion complete. Output saved to $output_file"
+}
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 -f <input_file> | -d <input_directory>"
+    exit 1
+fi
+
+option="$1"
+input_path="$2"
+
+# List of supported video extensions
+video_extensions="avi mkv mp4 m4v ts vid"
+
+# Function to check if a file has a valid video extension
+is_video_file() {
+    local file="$1"
+    local extension="${file##*.}"
+    for ext in $video_extensions; do
+        if [[ "$extension" == "$ext" ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+if [ "$option" == "-f" ]; then
+    if [ -f "$input_path" ]; then
+        if is_video_file "$input_path"; then
+            convert_file "$input_path"
+        else
+            echo "Error: $input_path is not a valid video file."
+            exit 1
+        fi
+    else
+        echo "Error: $input_path is not a valid file."
+        exit 1
+    fi
+elif [ "$option" == "-d" ]; then
+    if [ -d "$input_path" ]; then
+        for file in "$input_path"/*; do
+            if [ -f "$file" ] && is_video_file "$file"; then
+                convert_file "$file"
+            fi
+        done
+    else
+        echo "Error: $input_path is not a valid directory."
+        exit 1
+    fi
+else
+    echo "Usage: $0 -f <input_file> | -d <input_directory>"
+    exit 1
+fi
