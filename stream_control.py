cebb376bc9029aa62bbd83a13b4bb46c75f9c1ec
teklynk <teklynk@protonmail.com>
2026-01-07 23:47:28 -0500

    fixed issue where random recordings were being remuxed instead of the the latest file

--- stream_control.py
+++ stream_control.py
@@ -498,7 +498,7 @@ def stop_recording():
         record_process = None
 
         # Remux the recording
-        recording_file = glob.glob('recordings/recording_*.mp4')[-1]  # Get the latest recording file
+        recording_file = sorted(glob.glob('recordings/recording_*.mp4'))[-1]  # Get the latest recording file
         logging.debug("Remuxing recording...")
         remux_file = f"recordings/recording_{int(time.time())}_remuxed.mp4"
         remux(recording_file, remux_file)
@@ -574,7 +574,7 @@ def stop_stream_recording():
         stream_record_process = None
 
         # Remux the recording
-        recording_file = glob.glob('recordings/stream_*.mp4')[-1]  # Get the latest recording file
+        recording_file = sorted(glob.glob('recordings/stream_*.mp4'))[-1]  # Get the latest recording file
         logging.debug("Remuxing recording...")
         remux_file = f"recordings/stream_{int(time.time())}_remuxed.mp4"
         remux(recording_file, remux_file)
